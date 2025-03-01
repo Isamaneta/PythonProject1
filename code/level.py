7fb15c90132139c6d4d130200d814e6891687eb6
Isabella <isabella.bruna22@hotmail.com>
2025-03-01 16:36:23 +0000

    Adding classes generated from StarUML

---
+++ code/level.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Game import Game
+
+
+class Level(Game):
+    def __init__(self):
+        self.window = None
+        self.name = None
+        self.entity_list = None
+
+    def run(self, ):
+        pass
