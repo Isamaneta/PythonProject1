9469d884224a198111c12785b2008caf2097769e
Isabella <isabella.bruna22@hotmail.com>
2025-03-08 13:10:19 +0000

    Creating menu interations Creating ABC class Creating Factory Level 1 Bg implementation: parallax

--- code/level.py
+++ code/level.py
@@ -12,13 +12,14 @@ class Level:
         self.name = name
         self.game_mode = game_mode # Mode game
         self.entity_list: list[Entity] = []
-        self.entity_list.extend(EntityFactory.get_entity('LevelBg1'))
+        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
 
 
-    def run(self, ):
+    def run(self):
         while True:
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
+                ent.move()
             pygame.display.flip()
         pass
 
--- code/menu.py
+++ code/menu.py
@@ -10,7 +10,7 @@ from code.Const import WIN_WIDTH, COLOR_BLACK, MENU_OPTION, C_BLUE, COLOR_ORANGE
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./asset/MenuBg.png')
+        self.surf = pygame.image.load('./asset/MenuBg1.png')
         self.rect = self.surf.get_rect(left=0, top=0)
 
 
